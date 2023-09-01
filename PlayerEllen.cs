eb1cc8a2dce65f5e5e86efdd901f93565f29ccf6
Selin Şeker <116524939+selinseker@users.noreply.github.com>
2023-09-01 13:40:58 +0300

    Add files via upload

---
+++ PlayerEllen.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerTop : MonoBehaviour
+{
+    private float MoveSpeed = 11f;
+    private Rigidbody rb;
+
+    Animator playerAnimator;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        playerAnimator = GetComponent<Animator>();
+    }
+
+    
+    void Update()
+    {
+
+      
+    }
+
+    private void FixedUpdate()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(-horizontalInput, 0.0f, -verticalInput).normalized * MoveSpeed;
+
+        rb.velocity = movement;
+
+        if (movement != Vector3.zero)
+        {
+            
+            Quaternion newRotation = Quaternion.LookRotation(movement);
+            transform.rotation = Quaternion.Slerp(transform.rotation, newRotation, 0.15f);
+
+           
+            playerAnimator.SetBool("PlayerSpeed", true);
+        }
+        else
+        {
+            playerAnimator.SetBool("PlayerSpeed", false);
+        }
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Mermi"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
+        }
+        
+    }
+
+}
