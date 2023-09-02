0068b1874201d8a089c8d5c87a24f8ee5a6c8708
Selin Şeker <116524939+selinseker@users.noreply.github.com>
2023-09-02 20:32:17 +0300

    Add files via upload

---
+++ EnemyChomperScript.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyChomperScript : MonoBehaviour
+{
+    public bool NearChomper = false;
+
+    Animator ChomperAnim;
+
+    Transform player;
+
+
+    void Start()
+    {
+        ChomperAnim = gameObject.GetComponent<Animator>();
+        player = GameObject.FindGameObjectWithTag("Player").transform; 
+    }
+
+
+    void Update()
+    {
+        Vector3 direction = player.position - transform.position;
+        Quaternion lookRotation = Quaternion.LookRotation(direction);
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            ChomperAnim.SetBool("NearChomper", true);
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            ChomperAnim.SetBool("NearChomper", false);
+        }
+    }
+}
+
+
+    
+
