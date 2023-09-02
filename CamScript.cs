deed53dbdff8df9f65841efc63033433a917e555
Selin Şeker <116524939+selinseker@users.noreply.github.com>
2023-09-03 00:26:18 +0300

    Update CamScript.cs

--- CamScript.cs
+++ CamScript.cs
@@ -4,27 +4,23 @@ using UnityEngine;
 
 public class CamScript : MonoBehaviour
 {
-    public Transform player; // Oyuncunun Transform bileşeni
+    public Transform player; 
 
-    public Vector3 offset = new Vector3(0f, 5f, -10f); // Kamera ile oyuncu arasındaki mesafe
-
-    public float smoothSpeed = 0.5f; // Kamera takip hızı
+    private Vector3 offset = new Vector3(0f, 4.5f, 8f); 
+ 
 
     void Start()
     {
-        
+
     }
 
-    // Update is called once per frame
+   
     void Update()
     {
         Vector3 desiredPosition = player.position + offset;
-        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
-        transform.position = smoothedPosition;
-
+        //Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
+        transform.position = desiredPosition;
         transform.LookAt(player);
     }
-    
 
-   
 }
